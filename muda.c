9941c67e4d740b0dc3bb29d8d529deba477dd83c
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2016-01-23 01:37:50 +0900

    first commit for muda proj

---
+++ muda.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void rm_muda(const char *name) {
+  char cmd[strlen(name)+7];
+  printf("rm %s...", name);
+  sprintf(cmd, "rm -rf %s", name);
+  system(cmd);
+  printf("complete\n");
+}
+
+int main(int argc, char **argv) {
+  FILE *fp;
+  char path[1000];
+  if ((fp = fopen(argv[1],"r")) == NULL) {
+    fprintf(stderr,"cannot open FILE: %s", argv[1]);
+  }
+
+  while((fgets(path, 1000, fp)) != NULL) {
+    rm_muda(path);
+  }
+
+  return 0;
+}
